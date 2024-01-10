cf89cfde553d446b5ad812904c46d21ac7f398ab
Mourent <jmourent@student.ciputra.ac.id>
2024-01-10 10:30:16 +0700

    Initial Commit

---
+++ FarmBuddy/FarmBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmBuddyApp.swift
+//  FarmBuddy
+//
+//  Created by MacBook Pro on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FarmBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
